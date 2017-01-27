8400c84aa9736528ff4d01b26de6e2e7da29d4f6
tomixnscale89 <tomixnscale89@gmail.com>
2017-01-27 01:04:00 -0800

    added demo concept map

---
+++ resources/Terrain/convert_democoncept.bat
@@ -0,0 +1,4 @@
+D:\NITRO-SDK\NitroSDK\tools\bin\ntexconv.exe -b -f palette256 C:\Users\Tomix\Documents\GitHub\TrainDS\resources\Terrain\demo_concept.bmp
+copy demo_concept.ntft terrain.hmap
+del demo_concept.ntft
+del demo_concept.ntfp
